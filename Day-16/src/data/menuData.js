e79dde91c4dcfbf6da3e9f7587ece0d41ee27af0
khushi <khushitanwer227@gmail.com>
2026-06-24 16:10:55 +0530

    Add files via upload

---
+++ Day-16/src/data/menuData.js
@@ -0,0 +1,28 @@
+export const menuData = {
+    Men:[
+        "Featured",
+         "Footwear",
+         "Clothing",
+         "Accessories",
+         "Sports"
+    ],
+
+    WOMEN:[
+        "Featured",
+         "Footwear",
+         "Clothing",
+         "Accessories",
+         "Sports"
+    ],
+
+    KIDS:[
+        "Featured",
+         "Footwear",
+         "Clothing",
+         "Accessories",
+         "Sports"
+    ],
+}
+
+
+
