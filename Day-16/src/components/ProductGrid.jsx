7b79a5ee30a531da1070729641567904882b4f5e
khushi <khushitanwer227@gmail.com>
2026-06-24 23:53:50 +0530

    ProductGrid

--- Day-16/src/components/ProductGrid.jsx
+++ Day-16/src/components/ProductGrid.jsx
@@ -1,6 +1,6 @@
 
 import React from 'react'
-import './Product.css'
+import styles from'./Product.module.css'
 
 const OUTLET_MOCKS = [
   { id: 1, name: "SAMBA OG SHOES", category: "Originals", basePrice: 10999, promoPrice: 7699, discount: "-30%", image: "https://assets.myntassets.com/w_412,q_50,,dpr_3,fl_progressive,f_webp/assets/images/26518298/2024/4/26/e86cd3ce-01c8-4118-b6fa-055e1a65e8af1714107388783-ADIDAS-Originals-Men-Casual-Shoes-8301714107388512-1.jpg" },
@@ -11,26 +11,25 @@ const OUTLET_MOCKS = [
 
  function ProductGrid() {
   return (
-    <section className="catalogContainer">
-      <h2 className="gridHeader">LIVE EOSS MARKDOWNS</h2>
-      <div className="gridSchema">
+    <section  className={styles.catalogContainer}>
+      <h2 className={styles.gridHeader}>LIVE EOSS MARKDOWNS</h2>
+      <div className={styles.gridSchema}>
         {OUTLET_MOCKS.map((item) => (
-          <div key={item.id} className="productCard">
+          <div key={item.id} className={styles.productCard}>
           
-            <div className="frameBox">
-              <img src={item.image} alt={item.name} className="assetImg"/>
-              <span className="badge">{item.discount}</span>
-              <button className="heartBtn">❤️</button>
+            <div className={styles.frameBox}>
+              <img src={item.image} alt={item.name} className={styles.assetImg}/>
+              <span className={styles.badge}>{item.discount}</span>
+              <button className={styles.heartBtn}>❤️</button>
             </div>
 
-          
-            <div className="metaData">
-              <div className="priceRow">
-                <span className="activePrice">₹{item.promoPrice.toLocaleString('en-IN')}</span>
-                <span className="basedPrice">₹{item.basePrice.toLocaleString('en-IN')}</span>
+          <div className={styles.metaData}>
+              <div className={styles.priceRow}>
+                <span className={styles.activePrice}>₹{item.promoPrice.toLocaleString('en-IN')}</span>
+                <span className={styles.basedPrice}>₹{item.basePrice.toLocaleString('en-IN')}</span>
               </div>
-              <h3 className="titleText">{item.name}</h3>
-              <p className="subText">{item.category}</p>
+              <h3 className={styles.titleText}>{item.name}</h3>
+              <p className={styles.subText}>{item.category}</p>
             </div>
 
           </div>
