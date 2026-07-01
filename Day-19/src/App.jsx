1ffbf10ba190068165770562d6d757959876272b
khushi <khushitanwer227@gmail.com>
2026-07-01 12:34:02 +0530

    Add files via upload

---
+++ Day-19/src/App.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import {  Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import Home from './pages/Home';
+import Products from './pages/Products/Products';
+import ProductDetail from './pages/ProductDetail/ProductDetail';
+import Cart from './pages/Cart/Cart'; // Import Cart Page
+// Sahi path check karein (aapke folder structure ke hisab se)
+import { CartProvider } from "./context/CartContext";
+
+// function App() {
+//   return (
+
+//       <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+//         <Navbar />
+        
+     
+//         <div style={{ flex: 1 }}>
+//           <Routes>
+//             <Route path="/" element={<Navigate to="/home" />} />
+//             <Route path="/home" element={<Home />} />
+//             <Route path="/products/:categoryName" element={<Products />} />
+//             <Route path="/products" element={<Products />} />
+//             <Route path="/product/:product_id" element={<ProductDetail />} />
+//           </Routes>
+//         </div>
+
+//         <Footer />
+//       </div>
+   
+//   );
+// }
+
+// export default App;
+
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+// import { CartProvider } from './context/CartContext'; // Import Provider
+// import Navbar from './components/Navbar';
+// import Footer from './components/Footer';
+// import Home from './pages/Home';
+// import Products from './pages/Products';
+// import ProductDetail from './pages/ProductDetail';
+//import Cart from './pages/Cart'; // Import Cart Page
+
+function App() {
+  return (
+    <CartProvider> 
+   
+        <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+          <Navbar />
+          
+          <div style={{ flex: 1 }}>
+            <Routes>
+              <Route path="/" element={<Navigate to="/home" />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/products/:categoryName" element={<Products />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/product/:product_id" element={<ProductDetail />} />
+              <Route path="/cart" element={<Cart />} /> {/* Cart Page Route */}
+            </Routes>
+          </div>
+
+          <Footer />
+        </div>
+    
+    </CartProvider>
+  );
+}
+
+export default App;
+
