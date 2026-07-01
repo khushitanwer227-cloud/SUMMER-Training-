8c60684c299a49a342d3e00eb83ecd31edc35295
khushi <khushitanwer227@gmail.com>
2026-07-01 17:47:10 +0530

    App

--- Day-19/src/App.jsx
+++ Day-19/src/App.jsx
@@ -6,7 +6,6 @@ import Home from './pages/Home';
 import Products from './pages/Products/Products';
 import ProductDetail from './pages/ProductDetail/ProductDetail';
 import Cart from './pages/Cart/Cart'; 
-
 import { CartProvider } from "./context/CartContext";
 
 
