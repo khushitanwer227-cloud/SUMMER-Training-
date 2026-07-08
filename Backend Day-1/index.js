2eb83fe7cdf54ea379f3b4ae7d3609c093a13e04
khushi <khushitanwer227@gmail.com>
2026-07-08 13:22:38 +0530

    index.js
    Removed commented-out routes and fixed syntax errors in user-related routes.

--- Backend Day-1/index.js
+++ Backend Day-1/index.js
@@ -14,9 +14,6 @@ app.get('/',(req,res)=>{
 })
 
 
-// app.get('/',(req,res)=>{
-//     res.send("listens 5000")
-// })
 
 app.get('/',(req,res)=>{
     res.send("<h1> Welcome to Home Page</h1>")
@@ -28,4 +25,48 @@ app.get('/about',(req,res)=>{
 
 app.get('/gallery',(req,res)=>{
     res.send("<h1> Gallery Page</h1>")
+})
+
+
+// app.get('/random.text',(req,res)=>{
+//     res.send("<h1>Random Page</h1>")
+// })
+//nested route
+app.get('/anout/user',(req,res)=>{
+    res.send("<h1>User Page</h1>")
+})
+
+//parameters
+// app.get('/about/:id',(req,res)=>{
+//     res.send(req.params)
+// })
+
+app.get('/user/:userid/book/:bookid',(req,res)=>{
+    res.send(req,params)
+})
+//single 
+app.get('/user/:userid/book/:bookid',(req,res)=>{
+    res.send("User Id : " + req,params.userid)
+})
+
+
+app.get('/user/:userid-bookid',(req,res)=>{
+    res.send(req,params)
+})
+
+
+//query parameters  ->  localhost:5000/search?name=khushi
+ //multiple ->  localhost:5000/search?name=anil&age=20&city=chandigarh
+ 
+app.get('/search',(req,res)=>{
+    res.send(req,query)
+})
+
+
+app.get('/search',(req,res)=>{
+
+    const name = req.query.name
+    const age = req.query.age
+
+    res.send(`Search results for Name : ${name} ,Age:${age}`)
 })
